9382c338cf86e6d8620feda209d1ef3e4b9e3a10
claudiodamico <cdamico18@gmail.com>
2022-01-28 12:04:32 -0300

    Backend

---
+++ PeliculasAPI/PeliculasAPI/Controllers/GenerosController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PeliculasAPI.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PeliculasAPI.Controllers
+{
+    [Route("api/generos")]
+    [ApiController]
+    //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class GenerosController : ControllerBase
+    {
+        private readonly ILogger<GenerosController> logger;
+
+        public GenerosController(ILogger<GenerosController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]  
+        public ActionResult<List<Genero>> Get()
+        {
+            return new List<Genero>() { new Genero() { Id =1, Nombre = "Comedia"} };
+            
+        }
+
+        [HttpGet("{Id:int}")] 
+        public async Task<ActionResult<Genero>> Get(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Genero genero)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public ActionResult Put([FromBody] Genero genero)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        public ActionResult Delete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
